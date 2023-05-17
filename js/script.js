dafffcda214debbd557219852b16a901764237a1
Abhi_01 <abhishek.jadhao.er@gmail.com>
2023-05-17 10:23:35 +0530

    36.40th min

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
+const option_list = document.querySelector(".option_list");
 
 //  Start If Quiz Button
 
@@ -34,13 +35,13 @@ let next_btn = quiz_box.querySelector(".next_btn");
 
 // If Next Button Clicked
 next_btn.onclick = () => {
-  if (que_count > questions.length - 1) {
-    console.log("Questions Completed");
-  } else {
+  if (que_count < questions.length - 1) {
     que_count++;
     que_numb++;
     showQuestion(que_count);
     queCounter(que_numb);
+  } else {
+    console.log("Questions Completed");
   }
 };
 
@@ -66,27 +67,47 @@ function showQuestion(index) {
     "<span></span></div>" +
     '<div class="option">' +
     questions[index].options[3] +
-    "<span></span></div>";
+    "</ span></div>";
   que_text.innerHTML = que_tag;
   option_list.innerHTML = option_tag;
 
   const option = option_list.querySelectorAll(".option");
-  for (let i = 0; i < option_list.length; i++) {
+  for (let i = 0; i < option.length; i++) {
     option[i].setAttribute("onclick", "optionSelected(this)");
   }
 }
 
-// function optionSelected(answer) {
-//   let userAns = answer.textContent();
-//   console.log(userAns);
-// }
+function optionSelected(answer) {
+  let userAns = answer.textContent;
+  let correctAns = questions[que_count].answer;
+  let allOptions = option_list.children.length;
+  if (userAns == correctAns) {
+    answer.classList.add("correct");
+    console.log("Answer is correct");
+  } else {
+    answer.classList.add("incorrect");
+    console.log("Answer is wrong");
+
+    // if answer is incorrect, then automatically show correct answer
+    for (let i = 0; i < allOptions; i++) {
+      if (option_list.children[i].textContent == correctAns) {
+        option_list.children[i].setAttribute("class", "option correct");
+      }
+    }
+  }
+
+  // once user selected option, disable another options
+  for (let i = 0; i < allOptions; i++) {
+    option_list.children[i].classList.add("disabled");
+  }
+}
 
 function queCounter(index) {
   const bottom_ques_counter = quiz_box.querySelector(".total_que");
   let totalQuesCountTag =
     " <span><p>" +
     index +
-    "</p>Of<p>" +
+    "</p>of<p>" +
     questions.length +
     "</p>Questions</span>";
   bottom_ques_counter.innerHTML = totalQuesCountTag;
