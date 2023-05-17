abfd39e76462e1722630daccdda173408821c6f6
Abhi_01 <abhishek.jadhao.er@gmail.com>
2023-05-17 14:54:32 +0530

    41st min

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
 const option_list = document.querySelector(".option_list");
+const timeCount = quiz_box.querySelector(".timer .timer_sec");
 
 //  Start If Quiz Button
 
@@ -26,10 +27,12 @@ continue_btn.onclick = () => {
   quiz_box.classList.add("activeQuiz"); // Show thw Quiz Box
   showQuestion(0);
   queCounter(1);
+  startTimer(15);
 };
 
 let que_count = 0;
 let que_numb = 1;
+let counter;
 
 let next_btn = quiz_box.querySelector(".next_btn");
 
@@ -77,6 +80,9 @@ function showQuestion(index) {
   }
 }
 
+let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
 function optionSelected(answer) {
   let userAns = answer.textContent;
   let correctAns = questions[que_count].answer;
@@ -84,14 +90,16 @@ function optionSelected(answer) {
   if (userAns == correctAns) {
     answer.classList.add("correct");
     console.log("Answer is correct");
+    answer.insertAdjacentHTML("beforeend", tickIcon);
   } else {
     answer.classList.add("incorrect");
     console.log("Answer is wrong");
-
+    answer.insertAdjacentHTML("beforeend", crossIcon);
     // if answer is incorrect, then automatically show correct answer
     for (let i = 0; i < allOptions; i++) {
       if (option_list.children[i].textContent == correctAns) {
         option_list.children[i].setAttribute("class", "option correct");
+        option_list.children[i].insertAdjacentHTML("beforeend", tickIcon);
       }
     }
   }
@@ -99,6 +107,14 @@ function optionSelected(answer) {
   // once user selected option, disable another options
   for (let i = 0; i < allOptions; i++) {
     option_list.children[i].classList.add("disabled");
+  }
+}
+
+function startTimer(time) {
+  counter = setInterval(timer, 1000);
+  function timer() {
+    timeCount.textContent = time;
+    time--;
   }
 }
 
